e55e87048a658ac972e42475bbe70ce5a366139e
Stagiaire <stagiaire@iMac-de-Stagiaire.local>
2017-03-17 15:07:14 +0100

    Initial Commit

---
+++ CodeBarre/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodeBarre
+//
+//  Created by Stagiaire on 17/03/2017.
+//  Copyright © 2017 Stagiaire. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
